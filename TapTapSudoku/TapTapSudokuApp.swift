f7f3dbf0b8c8b85624fa47f54a6aeb1410bcd4b2
Francisco Serrano <franciscoserrano@Franciscos-MBP.attlocal.net>
2023-01-21 20:45:42 -0800

    initial commit

---
+++ TapTapSudoku/TapTapSudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapTapSudokuApp.swift
+//  TapTapSudoku
+//
+//  Created by Francisco Serrano on 1/21/23.
+//
+
+import SwiftUI
+
+@main
+struct TapTapSudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
